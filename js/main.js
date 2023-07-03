efd2d40b8cd7b5eb0c8beeab5b4606032690e0f8
[joao miguel] <83466001+joao-miguel8@users.noreply.github.com>
2023-07-02 22:00:19 -0400

    - forgot to push project has been completed since almost 2 months ago

--- js/main.js
+++ js/main.js
@@ -1,8 +1,18 @@
-const searchButton = document.querySelector(".location-search").addEventListener("click", searchForLocation);
+const searchButtonEle = document.querySelector(".location-search");
+const mainTemperatureEle = document.querySelector(".main-temp");
+
+// Metric values:
+const windSpeedValueEle = document.querySelector(".wind-speed-value");
+const feelsLikeValueEle = document.querySelector(".feels-like-value");
+const humidityValueEle = document.querySelector(".humidity-value");
+const windDegreeValueEle = document.querySelector(".wind-degrees-value");
+
+searchButtonEle.addEventListener("click", searchForLocation);
 
 const searchInputEle = document.querySelector(".location-input");
 
-function searchForLocation() {
+function searchForLocation(e) {
+    e.preventDefault();
     const apiKey = "xE/RhVYt/M7ZnjK7p2aHoQ==16REn2WYPkE0mDeb";
     const searchValue = searchInputEle.value;
     const url = `https://api.api-ninjas.com/v1/weather?city=${searchValue}`;
@@ -10,7 +20,15 @@ function searchForLocation() {
         method: `GET`,
         headers: { "X-Api-Key": `${apiKey}` },
     })
-        .then((res) => res.json())
-        .then((data) => console.log(data))
+        .then((response) => response.json())
+        .then((data) => {
+            console.log(data);
+            mainTemperatureEle.textContent = data.temp;
+            windSpeedValueEle.textContent = `${data.wind_speed}km`;
+            feelsLikeValueEle.textContent = `${data.wind_speed}km`;
+            humidityValueEle.textContent = `${data.humidity}km`;
+            windDegreeValueEle.textContent = `${data.wind_speed}km`;
+        })
         .catch((err) => console.log(err));
+    searchInputEle.value = "";
 }
